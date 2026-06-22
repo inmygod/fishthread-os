cadb69f855a3ca060ac391e69afd577cec14e3ba
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-22 15:35:22 +0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        fontFamily: "sans-serif"
+      }}
+    >
+      <h1>FishThread OS</h1>
+
+      <p>
+        Offline First Fish Trading Operating System
+      </p>
+
+      <p>
+        Build Authority Active
+      </p>
+    </div>
+  );
+}
