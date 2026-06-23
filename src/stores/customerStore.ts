86a1d7ea7b3c3d8bafa8f791f561ae0118948af5
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-23 22:40:41 +0600

    Update customerStore.ts

--- src/stores/customerStore.ts
+++ src/stores/customerStore.ts
@@ -1,1 +1,72 @@
-export {};
+import { create } from "zustand";
+import { Customer } from "../types/Customer";
+
+interface CustomerStore {
+  customers: Customer[];
+
+  addCustomer: (customer: Customer) => void;
+
+  updateCustomer: (
+    id: string,
+    customer: Partial<Customer>
+  ) => void;
+
+  archiveCustomer: (id: string) => void;
+
+  deleteCustomer: (id: string) => void;
+}
+
+export const useCustomerStore =
+  create<CustomerStore>((set) => ({
+    customers: [],
+
+    addCustomer: (customer) =>
+      set((state) => ({
+        customers: [
+          ...state.customers,
+          customer,
+        ],
+      })),
+
+    updateCustomer: (
+      id,
+      updatedCustomer
+    ) =>
+      set((state) => ({
+        customers:
+          state.customers.map((customer) =>
+            customer.id === id
+              ? {
+                  ...customer,
+                  ...updatedCustomer,
+                }
+              : customer
+          ),
+      })),
+
+    archiveCustomer: (id) =>
+      set((state) => ({
+        customers:
+          state.customers.map((customer) =>
+            customer.id === id
+              ? {
+                  ...customer,
+                  archived: true,
+                }
+              : customer
+          ),
+      })),
+
+    deleteCustomer: (id) =>
+      set((state) => ({
+        customers:
+          state.customers.map((customer) =>
+            customer.id === id
+              ? {
+                  ...customer,
+                  deleted: true,
+                }
+              : customer
+          ),
+      })),
+  }));
