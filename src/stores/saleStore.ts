6cfc1a898d1aaacd462bf7180afc7b3d7cab14f5
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-23 22:45:18 +0600

    Update saleStore.ts

--- src/stores/saleStore.ts
+++ src/stores/saleStore.ts
@@ -1,1 +1,78 @@
-export {};
+import { create } from "zustand";
+import { SaleInvoice } from "../types/SaleInvoice";
+
+interface SaleStore {
+  sales: SaleInvoice[];
+
+  addSale: (
+    sale: SaleInvoice
+  ) => void;
+
+  updateSale: (
+    id: string,
+    sale: Partial<SaleInvoice>
+  ) => void;
+
+  archiveSale: (
+    id: string
+  ) => void;
+
+  deleteSale: (
+    id: string
+  ) => void;
+}
+
+export const useSaleStore =
+  create<SaleStore>((set) => ({
+    sales: [],
+
+    addSale: (sale) =>
+      set((state) => ({
+        sales: [
+          ...state.sales,
+          sale,
+        ],
+      })),
+
+    updateSale: (
+      id,
+      updatedSale
+    ) =>
+      set((state) => ({
+        sales:
+          state.sales.map((sale) =>
+            sale.id === id
+              ? {
+                  ...sale,
+                  ...updatedSale,
+                }
+              : sale
+          ),
+      })),
+
+    archiveSale: (id) =>
+      set((state) => ({
+        sales:
+          state.sales.map((sale) =>
+            sale.id === id
+              ? {
+                  ...sale,
+                  archived: true,
+                }
+              : sale
+          ),
+      })),
+
+    deleteSale: (id) =>
+      set((state) => ({
+        sales:
+          state.sales.map((sale) =>
+            sale.id === id
+              ? {
+                  ...sale,
+                  deleted: true,
+                }
+              : sale
+          ),
+      })),
+  }));
