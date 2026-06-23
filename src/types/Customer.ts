7bb0f82182d88df9f8734bdf01d4d32f1bd691c4
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-23 10:42:37 +0600

    Update Customer.ts

--- src/types/Customer.ts
+++ src/types/Customer.ts
@@ -1,1 +1,19 @@
-export interface Customer {}
+export interface Customer {
+  id: string;
+
+  name: string;
+
+  phone?: string;
+
+  address?: string;
+
+  notes?: string;
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  archived: boolean;
+
+  deleted: boolean;
+}
