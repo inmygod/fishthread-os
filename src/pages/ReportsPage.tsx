cfa43fada8ecc0c843b660b8b30b8c81354a5086
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 08:01:49 +0600

    Update ReportsPage.tsx

--- src/pages/ReportsPage.tsx
+++ src/pages/ReportsPage.tsx
@@ -1,3 +1,11 @@
 export default function ReportsPage() {
-  return <h1>রিপোর্ট</h1>;
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>রিপোর্ট</h1>
+
+      <p>
+        ব্যবসার রিপোর্ট
+      </p>
+    </div>
+  );
 }
