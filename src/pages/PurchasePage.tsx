17f974b339460f6e705983747969ac303f8047b3
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 07:59:08 +0600

    Update PurchasePage.tsx

--- src/pages/PurchasePage.tsx
+++ src/pages/PurchasePage.tsx
@@ -1,3 +1,12 @@
 export default function PurchasePage() {
-  return <h1>ক্রয়</h1>;
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>ক্রয়</h1>
+
+      <p>
+        নতুন ক্রয় চালান তৈরি
+        করুন।
+      </p>
+    </div>
+  );
 }
