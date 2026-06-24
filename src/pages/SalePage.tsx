76ec1cf884e58018a524ca4a484c0e2c5f989719
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 07:59:59 +0600

    Update SalePage.tsx

--- src/pages/SalePage.tsx
+++ src/pages/SalePage.tsx
@@ -1,3 +1,12 @@
 export default function SalePage() {
-  return <h1>বিক্রয়</h1>;
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>বিক্রয়</h1>
+
+      <p>
+        নতুন বিক্রয় চালান তৈরি
+        করুন।
+      </p>
+    </div>
+  );
 }
