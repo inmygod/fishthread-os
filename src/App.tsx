77324a08b63445639a9568811e1f471fd1b2540c
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 07:54:52 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
+
 import DashboardPage from "./pages/DashboardPage";
+import PurchasePage from "./pages/PurchasePage";
+import SalePage from "./pages/SalePage";
+import CustomersPage from "./pages/CustomersPage";
+import ReportsPage from "./pages/ReportsPage";
+import SettingsPage from "./pages/SettingsPage";
+
 import BottomNavigation from "./components/BottomNavigation";
 
 export default function App() {
+  const [activePage, setActivePage] =
+    useState("dashboard");
+
+  const renderPage = () => {
+    switch (activePage) {
+      case "purchase":
+        return <PurchasePage />;
+
+      case "sale":
+        return <SalePage />;
+
+      case "customers":
+        return <CustomersPage />;
+
+      case "reports":
+        return <ReportsPage />;
+
+      case "settings":
+        return <SettingsPage />;
+
+      default:
+        return <DashboardPage />;
+    }
+  };
+
   return (
-    <>
-      <DashboardPage />
-      <BottomNavigation />
-    </>
+    <div>
+      {renderPage()}
+
+      <BottomNavigation
+        activePage={activePage}
+        onNavigate={setActivePage}
+      />
+    </div>
   );
 }
