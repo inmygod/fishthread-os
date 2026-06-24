1ce2846580b32d499272230e3c96aa8503f37eb2
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 08:02:41 +0600

    Update SettingsPage.tsx

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,3 +1,11 @@
 export default function SettingsPage() {
-  return <h1>সেটিংস</h1>;
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>সেটিংস</h1>
+
+      <p>
+        সিস্টেম সেটিংস
+      </p>
+    </div>
+  );
 }
